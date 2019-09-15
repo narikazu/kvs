06f1167c306b47e2f5a17ba3c8ae17ce22a17bfe
Kazunari Okuda <saori.cube@gmail.com>
2019-09-15 16:53:54 +0200

    Finish the section Part 4

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,25 @@
+use std::collections::HashMap;
+
 pub struct KvStore {
+    hash_map: HashMap<String, String>,
 }
 
 impl KvStore {
     pub fn new() -> KvStore {
-        KvStore {}
+        KvStore {
+            hash_map: HashMap::new()
+        }
     }
 
     pub fn get(&self, key: String) -> Option<String> {
-        panic!()
+        self.hash_map.get(&key).cloned()
     }
 
-    pub fn set(&self, key: String, value: String) {
-        panic!()
+    pub fn set(&mut self, key: String, value: String) {
+        self.hash_map.insert(key, value);
     }
 
-    pub fn remove(&self, key: String) {
-        panic!()
+    pub fn remove(&mut self, key: String) {
+        self.hash_map.remove(&key);
     }
 }
