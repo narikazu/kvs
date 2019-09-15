43524f98562cdb23b4eb4dda750e8a6a618cfe2c
Kazunari Okuda <saori.cube@gmail.com>
2019-09-15 15:56:19 +0200

    Finish the section Part 2

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,33 @@
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+use std::process;
+
 fn main() {
-    println!("Hello world!");
+    let matches = App::new("kvs")
+        .version("0.1.0")
+        .subcommand(SubCommand::with_name("get")
+                    .arg(Arg::with_name("key")))
+        .subcommand(SubCommand::with_name("set")
+                    .arg(Arg::with_name("key"))
+                    .arg(Arg::with_name("value")))
+        .subcommand(SubCommand::with_name("rm")
+                    .arg(Arg::with_name("key")))
+        .get_matches();
+
+    match matches.subcommand_name() {
+        Some("get") => {
+            eprintln!("unimplemented");
+            process::exit(1);
+        },
+        Some("set") => {
+            eprintln!("unimplemented");
+            process::exit(1);
+        },
+        Some("rm") => {
+            eprintln!("unimplemented");
+            process::exit(1);
+        },
+        _ => { process::exit(1) },
+    }
 }
+
